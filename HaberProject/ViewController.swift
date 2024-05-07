ab0847dbfad2e51e1f04e69147687da3888d1139
Gülsüm Demirbaş <gdem2334@gmail.com>
2024-05-07 12:12:33 +0300

    Initial Commit

---
+++ HaberProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaberProject
+//
+//  Created by Gülsüm Demirbaş on 7.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
